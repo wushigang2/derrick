44695175fcc0c7fe2fbf3ada2ffb9b7bf02f9e6a
110allan <71679753+110allan@users.noreply.github.com>
2022-09-02 15:28:46 +0800

    Update pipeline.sh

--- data_files_insilico/pipeline.sh
+++ data_files_insilico/pipeline.sh
@@ -1,8 +1,8 @@
 # encode
-perl moban.pl -o vol3.txt -i children.mp4 -i pgzf -i ruan_group.jpg -i wtdbg2 -i wtdbg-cns -i wtpoa-cns > wcl.txt
-derrick encode -i pi.txt -n 255 -k 211 -s 62 vol3.txt > ref.211.fa
+perl merge.pl -o merge.txt -i playfun.mp4 -i pgzf -i group.jpg -i wtdbg2 -i wtdbg-cns -i wtpoa-cns > count.txt
+derrick encode -i pi.txt -n 255 -k 211 -s 62 merge.txt > ref.211.fa
 gzip ref.211.fa
 # decode
 gunzip ref.211.fa.gz
-derrick decode -i pi.txt -n 255 -k 211 -s 62 ref.211.fa > vol3.txt
-perl mom.pl -i1 wcl.txt -i2 vol3.txt -o1 children.mp4 -o2 pgzf -o3 ruan_group.jpg -o4 wtdbg2 -o5 wtdbg-cns -o6 wtpoa-cns
+derrick decode -i pi.txt -n 255 -k 211 -s 62 ref.211.fa > merge.txt
+perl split.pl -i1 count.txt -i2 merge.txt -o1 playfun.mp4 -o2 pgzf -o3 group.jpg -o4 wtdbg2 -o5 wtdbg-cns -o6 wtpoa-cns
